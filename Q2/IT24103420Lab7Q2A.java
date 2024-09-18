1047f7880584692d369b0cd14265fcbb5e36189e
It24103420 <it24103420@my.sliit.lk>
2024-09-18 10:09:52 +0530

    Add files via upload

---
+++ Q2/IT24103420Lab7Q2A.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class IT24103420Lab7Q2A
+{
+	public static void main(String[] args)
+	{
+		for (int i=1; i<=4; i++)
+		{
+			for (int j=1; j<=5; j++)
+			{
+				System.out.print("$ ");
+			}
+			System.out.println();
+		}
+	}
+}
+			
